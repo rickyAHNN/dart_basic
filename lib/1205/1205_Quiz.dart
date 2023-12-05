df22d5edd1926e4e475c5d091128dbaa27b9b3e1
rickyAHNN <smrmaster88@gmail.com>
2023-12-05 18:06:29 +0900

    1205_dart05.추상클래스와 인터페이스 연습문제

--- lib/1205/1205_Quiz.dart
+++ lib/1205/1205_Quiz.dart
@@ -8,10 +8,8 @@ PoisonSlime poisonSlime = PoisonSlime(‘A’);
 PoisonSlime 독 공격이 가능한 남은 횟수를 int 형 필드를 가지고 있고 초기값은 5 이다
 PoisonSlime attack() 메소드가 호출되면 다음 내용의 공격을 한다
 우선, “보통 슬라임과 같은 공격"을 한다
-“독 공격의 남은 횟수"가 0이 아니면 다음을 추가로 수행한다
-화면에 “추가로, 독 포자를 살포했다!” 를 표시
-용사의 HP 의 1/5에 해당하는 포인트를 용사의 HP 로부터 감소시키고, “~포인트의 데미지" 라고 표시
-“독 공격의 남은 횟수" 를 1 감소 시킨다
+
+
 
  */
 
@@ -40,15 +38,23 @@ class PoisonSlime extends Slime {
 
   @override
   void attack(Hero hero) {
+    /*
+    “독 공격의 남은 횟수"가 0이 아니면 다음을 추가로 수행한다
+화면에 “추가로, 독 포자를 살포했다!” 를 표시
+     */
     if (0 < usedPoison) {
       print('"추가로, 독 포자를 살포했다!"');
-
+/*
+용사의 HP 의 1/5에 해당하는 포인트를 용사의 HP 로부터 감소시키고, “~포인트의 데미지" 라고 표시
+“독 공격의 남은 횟수" 를 1 감소 시킨다
+ */
       int heroDamage = hero.hp ~/ 5;
       hero.hp -= heroDamage;
       print('"~포인트의 데미지"');
       usedPoison -= 1;
     }
     super.attack(hero);
+
   }
 }
 
---
+++ lib/1205/1205_Quiz2.dart
@@ -0,0 +1,88 @@
+
+/*
+컴퓨터, 책 을 표현하는, 다음과 같은 두개의 클래스생성
+유형자산(TangibleAsset) 이라는 이름의 추상클래스를 작성 하시오.
+또, Computer 나 Book 은 그 부모 클래스를 활용한 형태로 수정 하시오.
+ */
+class Book extends TangibleAsset {
+  String isbl;
+
+  Book(
+    super.weight, {
+    required super.name,
+    required super.price,
+    required super.color,
+    required this.isbl,
+  });
+}
+
+class Computer extends TangibleAsset {
+  String makerName;
+
+  Computer(
+    super.weight, {
+    required this.makerName,
+    required super.name,
+    required super.price,
+    required super.color,
+  });
+}
+
+
+/*
+자산인지 아닌지 따지지 말고, 형태가 있는 것 (Thing) 이면, 무게가 있다
+그래서, double 형으로 무게(weight)를 얻을 수 있도록 getter/setter를 가지는 인터페이스 Thing 을 만드시오
+유형자산 (TangibleAsset) 은, 자산 (Asset) 의 일종이며, 형태가 있는 것 (Thing) 의 일종이기도 하다.
+ */
+
+abstract interface class Thing {
+  double get weight;
+  set weight(double weight);
+}
+//무형자산도 유형자산도 자산(Asset)의 일종이다.
+abstract class Asset {
+  String name;
+  int price;
+
+  Asset({
+    required this.name,
+    required this.price,
+  });
+}
+/*
+또한, (가) 에 들어가는 추상 클래스를 개발하고, 이 클래스를 상속하도록 TangibleAsset 를 수정하시오.
+이 정의에 맞도록 TangibleAsset 의 소스 코드를 수정하시오.
+이 때, TangibleAsset 에 필드나 메소드의 추가가 필요하다면, 적당히 추가하시오.
+ */
+abstract class TangibleAsset extends Asset implements Thing {
+  //implements로 불러온것은 무조건 구현해야한다 오버라이드로
+  String color;
+//유형자산이기때문에 컬러
+  @override
+  //재정의
+  double weight;
+  TangibleAsset(
+    this.weight, {
+    required super.name,
+    required super.price,
+    required this.color,
+  });
+}
+//무형자산(IntangibleAsset) 도 관리하려고 생각하고 있다.
+abstract class IntangibleAsset extends Asset {
+  IntangibleAsset({
+    //무형자산이기때문에 컬러x
+    required super.name,
+    required super.price,
+  });
+}
+//무형자산에는, 예를들어 특허권(Patent) 등이 있다.
+abstract class Patent extends IntangibleAsset {
+  int quantity;
+  Patent({
+    required super.name,
+    required super.price,
+    required this.quantity,
+  });
+}
+
