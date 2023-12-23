f2af573e4f98764d13a1a91d492cc09831095607
rickyAHNN <smrmaster88@gmail.com>
2023-12-24 08:51:55 +0900

    전자액자 끝

---
+++ lib/mygallery/mygallery.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class MyGalleryApp extends StatefulWidget {
+  const MyGalleryApp({super.key});
+
+  @override
+  State<MyGalleryApp> createState() => _MyGalleryAppState();
+}
+
+class _MyGalleryAppState extends State<MyGalleryApp> {
+  final ImagePicker _picker = ImagePicker();
+  List<XFile>? images;
+  int currentPage = 0;
+  final pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    loadImages();
+  }
+
+  Future<void> loadImages() async {
+    images = await _picker.pickMultiImage();
+    if (images != null) {
+      Timer.periodic(const Duration(seconds: 5), (timer) {
+        currentPage++;
+
+        if (currentPage > images!.length - 1) {
+          currentPage = 0;
+        }
+        pageController.animateToPage(currentPage,
+            duration: const Duration(microseconds: 350), curve: Curves.easeIn);
+      });
+    }
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('전자액자'),
+        ),
+        body: images == null
+            ? const Center(child: Text('no data'))
+            : PageView(
+                controller: pageController,
+                children: images!.map((image) {
+                  return FutureBuilder<Uint8List>(
+                      future: image.readAsBytes(),
+                      builder: (context, snapshot) {
+                        final data = snapshot.data;
+
+                        if (data == null ||
+                            snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        return Image.memory(
+                          data,
+                          width: double.infinity,
+                        );
+                      });
+                }).toList(),
+              ));
+  }
+}
